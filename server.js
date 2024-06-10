f23640fe69195c749692dedc2d1fa8ee17444dbd
Manish Kumar Saini <144998072+manishmksaini@users.noreply.github.com>
2024-06-10 16:01:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const app = express();
 
 
 const conn = mongoose.connection;
-mongoose.connect(process.env.MONGO_CONNECTION_URL,{useNewUrlParser: true,})
+mongoose.connect("mongodb+srv://manishmksaini:mrmk%40291046@manishmksaini.rw8pnz0.mongodb.net/pizza",{useNewUrlParser: true,})
  .then(() => {
    console.log("connected successfully");
  })
